f078888c632ae988ed677ba36e472c90472c06ab
David Ward <admin@pineapplesoft.co.uk>
2021-01-25 10:28:31 +0000

    Barebones html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Booking Time</title>
+
+  <meta name="description" content="PHP, HTML5 and JavaScript flexible calendar booking system">
+  <meta name="author" content="PineappleSoft">
+
+  <!--
+  <link rel="stylesheet" href="style.css">
+
+  <script src="script.js"></script>
+  -->
+</head>
+
+<body>
+  <!-- page content -->
+</body>
+</html>
