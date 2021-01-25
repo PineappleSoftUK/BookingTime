d7aed8ad40d863e9ac86a1fc2d56e9e8b94be5a7
David Ward <admin@pineapplesoft.co.uk>
2021-01-25 10:31:01 +0000

    Add title

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+
+/**
+ * Main system page
+ *
+ */
+
+//Error reporting (Temporary during development)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Includes
+$includes = true;
+include_once __DIR__ . '/open_db.php';
+include __DIR__ . '/class/coord.php';
+
+//Create the coordinator
+$coord = new System\Coord($db);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
