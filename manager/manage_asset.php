d056d417a3017adeb0055e2cf21fc2896be81398
David Ward <admin@pineapplesoft.co.uk>
2021-03-28 10:04:45 +0100

    Correct error with breadcrumbs

--- manager/manage_asset.php
+++ manager/manage_asset.php
@@ -75,12 +75,6 @@ $assetArray = $coord->getAllAssets($selectedLocation);
 
   <title>Booking Time</title>
 
-  <ul class="breadcrumb">
-    <li><a href="../">Home</a></li>
-    <li><a href="index.php">Manager Hub</a></li>
-    <li>Manage Assets</li>
-  </ul>
-
   <meta name="description" content="PHP, HTML5 and JavaScript flexible calendar booking system">
   <meta name="author" content="PineappleSoft">
 
@@ -91,6 +85,12 @@ $assetArray = $coord->getAllAssets($selectedLocation);
 
 <body>
   <h1>Booking Time - Asset Manager</h1>
+
+  <ul class="breadcrumb">
+    <li><a href="../">Home</a></li>
+    <li><a href="index.php">Manager Hub</a></li>
+    <li>Manage Assets</li>
+  </ul>
 
   <?php echo $span; ?>
 
