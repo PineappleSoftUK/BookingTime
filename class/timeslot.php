739dc046ae2b17353023bf7050f7ca86c3f38b9e
David Ward <admin@pineapplesoft.co.uk>
2021-07-14 10:34:12 +0100

    Remove uneeded methods

--- class/timeslot.php
+++ class/timeslot.php
@@ -66,53 +66,6 @@ class TimeSlot
     return $this->time;
   }
 
-
-  /**
-  * New booking
-  *
-  * Adds a new booking to the database
-  *
-  */
-  public function newBooking()
-  {
-    $stmt = $this->db->prepare('INSERT INTO bookings (asset) VALUES (:asset)');
-    $stmt->bindValue(':asset', $this->id);
-    $result = $stmt->execute();
-  }
-
-  /**
-  * Get all bookings
-  *
-  * This returns an array of all booking objects linked this asset.
-  *
-  */
-  public function getAllbookings()
-  {
-    $bookingArray = array();
-    $stmt = $this->db->prepare('SELECT * FROM bookings WHERE asset = :asset');
-    $stmt->bindValue(':asset', $this->id);
-    $res = $stmt->execute();
-    while ($row = $res->fetchArray()) {
-      $object = new booking($this->db, $row['id'], $row['asset']);
-      array_push($bookingArray, $object);
-    }
-    return $bookingArray;
-  }
-
-  /**
-  * Delete booking function.
-  *
-  * This takes a booking object and removes it from the database.
-  *
-  */
-  public function deletebooking($bookingToDelete)
-  {
-    $stmt = $this->db->prepare('DELETE FROM bookings WHERE id = :id');
-    $stmt->bindValue(':id', $bookingToDelete->getID());
-    $result = $stmt->execute();
-  }
-
-
   /**
   * toString method.
   *
